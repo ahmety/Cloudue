37b79a76d6fd488a5f22027c452bfece9897f9a7
ahmety <ahmety26@hotmail.com>
2013-12-08 19:59:20 +0200

    Login View added and working

--- src/com/android/cloudue/LoginActivity.java
+++ src/com/android/cloudue/LoginActivity.java
@@ -2,7 +2,9 @@ package com.android.cloudue;
 
 import android.os.Bundle;
 import android.app.Activity;
+import android.content.Intent;
 import android.view.Menu;
+import android.view.View;
 
 public class LoginActivity extends Activity {
 
@@ -18,5 +20,10 @@ public class LoginActivity extends Activity {
 		getMenuInflater().inflate(R.menu.login, menu);
 		return true;
 	}
-
+	
+	public void login(View view){
+		Intent intent = new Intent(this, MainActivity.class);
+		startActivity(intent);
+	}
+	
 }
