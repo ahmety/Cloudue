5d420e65f95bcf53df7da5a0949b8cb7a259dc75
ahmety <ahmety26@hotmail.com>
2013-12-10 20:53:12 +0200

    event detail layout added

---
+++ src/com/android/cloudue/EventDetailsActivity.java
@@ -0,0 +1,27 @@
+package com.android.cloudue;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.View;
+
+public class EventDetailsActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_event_details);
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.event_details, menu);
+		return true;
+	}
+	
+	public void removeEvent(View view){
+		
+	}
+
+}
