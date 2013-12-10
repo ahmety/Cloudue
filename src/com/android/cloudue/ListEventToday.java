e459a5e6483470c06f4dd2384984a15f33c6004b
ahmetenis <eniserd63@gmail.com>
2013-12-10 20:25:53 +0200

    every list is up to date

--- src/com/android/cloudue/ListEventToday.java
+++ src/com/android/cloudue/ListEventToday.java
@@ -1,6 +1,9 @@
 package com.android.cloudue;
 
 import java.util.ArrayList;
+import java.util.List;
+
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.ListFragment;
@@ -9,33 +12,54 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 
-public class ListEventToday extends ListFragment{
-	
-	public final static String EXTRA_MESSAGE = "com.android.cloudue.MESSAGE";	
-	ArrayList <String> list_items;
+import com.parse.FindCallback;
+import com.parse.ParseObject;
+import com.parse.ParseQuery;
 
+public class ListEventToday extends ListFragment{
+	public final static String EXTRA_MESSAGE = "com.android.cloudue.MESSAGE";	
+	public Context context;
+	ArrayList<String> list_items;
+	
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		list_items = new ArrayList<String>();
+		ParseQuery<ParseObject> query = ParseQuery.getQuery("DueEvent");
+		query.whereEqualTo("listIndex", 0);
+		query.findInBackground(new FindCallback<ParseObject>() {
+
+			@Override
+			public void done(List<ParseObject> objects,
+					com.parse.ParseException e) {
+				if(e == null) {
+					for(ParseObject object : objects) {
+						list_items.add(object.getString("detail"));
+					}
+				}
+			}
+		});
+		
+		System.out.println("data is fetched");
+	}
+	
+	@Override
 	public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState){
 		View rootView = inflater.inflate(R.layout.list_today, container, false);
-		list_items = new ArrayList<String>();
-		//TODO: Each time app started, the contents of the list will be crawled from the cloud
-		list_items.add("CS 443 Midterm 2");
-		list_items.add("Attend to the project meeting");
-		list_items.add("Go to the Gym");
-		list_items.add("CS 491 Report");
-		list_items.add("Pay the Credit Card debt");
-		list_items.add("Return the books to Library");
-		setListAdapter(new ArrayAdapter<String>(getActivity(), android.R.layout.simple_list_item_1, list_items));
 		
-        rootView.findViewById(R.id.today_button)
-                .setOnClickListener(new View.OnClickListener() {
-                    @Override
-                    public void onClick(View view) {
-                        Intent intent = new Intent(getActivity(), AddEventActivity.class);
-                        String cameFrom = "0";
-                        intent.putExtra(EXTRA_MESSAGE, cameFrom);
-                        startActivity(intent);
-                    }
-                });		
+		System.out.println("on create view cagirildi");
+		ArrayAdapter<String> adapter = new ArrayAdapter<String>(getActivity(), android.R.layout.simple_list_item_1, list_items);
+		setListAdapter(adapter);
+		rootView.findViewById(R.id.today_button)
+        		.setOnClickListener(new View.OnClickListener() {
+		            @Override
+		            public void onClick(View view) {
+		                Intent intent = new Intent(getActivity(), AddEventActivity.class);
+		                String cameFrom = "0";
+		                intent.putExtra(EXTRA_MESSAGE, cameFrom);
+		                startActivity(intent);
+		            }
+		        });
 		return rootView;
 	}
 }
--- src/com/android/cloudue/ListEventTomorrow.java
+++ src/com/android/cloudue/ListEventTomorrow.java
@@ -1,7 +1,9 @@
 package com.android.cloudue;
 
 import java.util.ArrayList;
+import java.util.List;
 
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.ListFragment;
@@ -10,21 +12,46 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 
+import com.parse.FindCallback;
+import com.parse.ParseObject;
+import com.parse.ParseQuery;
+
 public class ListEventTomorrow extends ListFragment{
 	public final static String EXTRA_MESSAGE = "com.android.cloudue.MESSAGE";	
-	ArrayList <String> list_items;
+	public Context context;
+	ArrayList<String> list_items;
 	
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		list_items = new ArrayList<String>();
+		ParseQuery<ParseObject> query = ParseQuery.getQuery("DueEvent");
+		query.whereEqualTo("listIndex", 1);
+		query.findInBackground(new FindCallback<ParseObject>() {
+
+			@Override
+			public void done(List<ParseObject> objects,
+					com.parse.ParseException e) {
+				if(e == null) {
+					for(ParseObject object : objects) {
+						list_items.add(object.getString("detail"));
+					}
+				}
+			}
+		});
+		
+		System.out.println("data is fetched");
+	}
+	
+	@Override
 	public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState){
 		View rootView = inflater.inflate(R.layout.list_tomorrow, container, false);
-		list_items = new ArrayList<String>();
-		list_items.add("Update LinkedIn profile");
-		list_items.add("EEE 391 Assignment 3");
-		list_items.add("IE 400 Quiz 4");
-		list_items.add("CS 476 Homework 3");
-		setListAdapter(new ArrayAdapter<String>(getActivity(), android.R.layout.simple_list_item_1, list_items));
-		System.out.println("onCreateView called in tomorrow list");
-        rootView.findViewById(R.id.tomorrow_button)
-		        .setOnClickListener(new View.OnClickListener() {
+		
+		System.out.println("on create view cagirildi");
+		ArrayAdapter<String> adapter = new ArrayAdapter<String>(getActivity(), android.R.layout.simple_list_item_1, list_items);
+		setListAdapter(adapter);
+		rootView.findViewById(R.id.tomorrow_button)
+        		.setOnClickListener(new View.OnClickListener() {
 		            @Override
 		            public void onClick(View view) {
 		                Intent intent = new Intent(getActivity(), AddEventActivity.class);
@@ -32,9 +59,7 @@ public class ListEventTomorrow extends ListFragment{
 		                intent.putExtra(EXTRA_MESSAGE, cameFrom);
 		                startActivity(intent);
 		            }
-		        });	
-		
+		        });
 		return rootView;
 	}
-
 }
