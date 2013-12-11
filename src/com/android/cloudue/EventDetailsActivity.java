9248484058d219139b22851e0aae6f0cd1c44372
ahmetenis <eniserd63@gmail.com>
2013-12-11 19:53:18 +0200

    deletes an event from the list

--- src/com/android/cloudue/EventDetailsActivity.java
+++ src/com/android/cloudue/EventDetailsActivity.java
@@ -7,6 +7,11 @@ import android.view.Menu;
 import android.view.View;
 import android.widget.TextView;
 
+import com.parse.GetCallback;
+import com.parse.ParseException;
+import com.parse.ParseObject;
+import com.parse.ParseQuery;
+
 public class EventDetailsActivity extends Activity {
 
 	String eventName;
@@ -32,7 +37,25 @@ public class EventDetailsActivity extends Activity {
 	}
 	
 	public void removeEvent(View view){
-		
+		ParseQuery<ParseObject> query = new ParseQuery<ParseObject>("DueEvent");
+		query.whereEqualTo("listIndex", Integer.parseInt(listNo));
+		query.whereEqualTo("detail", eventName);
+		query.getFirstInBackground(new GetCallback<ParseObject>() {
+			
+			@Override
+			public void done(ParseObject object, ParseException e) {
+				// TODO Auto-generated method stub
+				if(object != null) {
+					System.out.println("icerde");
+					object.deleteInBackground();
+					System.out.println(object.getString("detail"));
+					System.out.println(object.getNumber("listIndex"));
+				}
+				Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+				intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+				startActivity(intent);
+			}
+		});
 	}
 
 }
