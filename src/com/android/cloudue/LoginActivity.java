5e3f7324914af05dd6e90f51963314ca5d3ef64f
ahmetenis <eniserd63@gmail.com>
2013-12-12 11:39:21 +0200

    Login is backed up
    exceptional cases are not considered thoroughly

--- src/com/android/cloudue/LoginActivity.java
+++ src/com/android/cloudue/LoginActivity.java
@@ -1,15 +1,25 @@
 package com.android.cloudue;
 
-import com.parse.Parse;
-
-import android.os.Bundle;
 import android.app.Activity;
 import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.preference.PreferenceManager;
 import android.view.Menu;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.parse.GetCallback;
+import com.parse.Parse;
+import com.parse.ParseException;
+import com.parse.ParseObject;
+import com.parse.ParseQuery;
 
 public class LoginActivity extends Activity {
-
+	String userName;
+	String password;
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
@@ -25,9 +35,40 @@ public class LoginActivity extends Activity {
 	}
 	
 	public void login(View view){
-		Intent intent = new Intent(this, MainActivity.class);
-		startActivity(intent);
-		finish();
+		EditText etUserName = (EditText)findViewById(R.id.username_login);
+		EditText etPassword = (EditText)findViewById(R.id.password_login);
+		userName = etUserName.getText().toString();
+		password = etPassword.getText().toString();
+		ParseQuery<ParseObject> query = new ParseQuery<ParseObject>("Users");
+		query.whereEqualTo("username", userName);
+		query.getFirstInBackground(new GetCallback<ParseObject>() {
+			
+			@Override
+			public void done(ParseObject object, ParseException e) {
+				// TODO Auto-generated method stub
+				if(object == null) {
+					Toast toast = Toast.makeText(getApplicationContext(), "Unknown user!", Toast.LENGTH_SHORT);
+					toast.show();
+				} else {
+					if(object.getString("password").equals(password)) {
+						SharedPreferences preferences = PreferenceManager.getDefaultSharedPreferences(getApplicationContext());
+						SharedPreferences.Editor editor = preferences.edit();
+						editor.clear();
+						editor.putString("userName", userName);
+						editor.commit();
+						
+						Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+						startActivity(intent);
+						finish();
+					}
+					else {
+						Toast toast = Toast.makeText(getApplicationContext(), "Wrong password!", Toast.LENGTH_SHORT);
+						toast.show();
+					}
+				}
+			}
+		});
+		
 	}
 	public void showSignupActivity(View view){
 		Intent intent = new Intent(this, SignupActivity.class);
