10fac9559931157b616caaea40e576794ccb69b4
ahmetenis <eniserd63@gmail.com>
2013-12-28 16:12:03 +0200

    little changes

--- src/com/android/cloudue/MainActivity.java
+++ src/com/android/cloudue/MainActivity.java
@@ -7,12 +7,8 @@ import android.os.Bundle;
 import android.preference.PreferenceManager;
 import android.support.v4.app.FragmentActivity;
 import android.support.v4.view.ViewPager;
-import android.view.ContextMenu;
-import android.view.ContextMenu.ContextMenuInfo;
 import android.view.Menu;
-import android.view.MenuInflater;
 import android.view.MenuItem;
-import android.view.View;
 
 import com.parse.Parse;
 
